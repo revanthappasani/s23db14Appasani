05e6c0145f45c1c9b9acf8a8f468c3fb762e3746
revanthappasani <143126804+revanthappasani@users.noreply.github.com>
2023-11-08 19:23:15 -0600

    added

--- controllers/Deer.js
+++ controllers/Deer.js
@@ -42,4 +42,24 @@ exports.Deer_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    // Handle Deer create on POST.
+exports.Deer_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Deer();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    document.Deer_color = req.body.Deer_color;
+    document.Deer_breed = req.body.Deer_breed;
+    document.Deer_price = req.body.Deer_price;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
     
