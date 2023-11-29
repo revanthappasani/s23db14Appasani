a900dcee2acbaba678725483ae03c43f4d91ba67
revanthappasani <143126804+revanthappasani@users.noreply.github.com>
2023-11-28 20:27:12 -0600

    added secure

--- routes/Deer.js
+++ routes/Deer.js
@@ -8,6 +8,15 @@
 var express = require('express');
 const Deer_controlers= require('../controllers/Deer');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 /* GET Deer */
 router.get('/', Deer_controlers.Deer_view_all_Page );
 /* GET detail costume page */
@@ -15,7 +24,7 @@ router.get('/detail', Deer_controlers.Deer_view_one_Page);
 /* GET create costume page */
 router.get('/create', Deer_controlers.Deer_create_Page);
 /* GET create update page */
-router.get('/update', Deer_controlers.Deer_update_Page);
+router.get('/update',secured, Deer_controlers.Deer_update_Page);
 /* GET delete costume page */
 router.get('/delete', Deer_controlers.Deer_delete_Page);
 module.exports = router;
